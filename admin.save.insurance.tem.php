09f6fb2844d0f153300d0944463fb4153a4a1ffb
Nick Lee <nlee@ascendosolutions.com>
2024-01-18 15:07:10 -0800

    Initial commit

---
+++ admin.save.insurance.tem.php
@@ -0,0 +1,32 @@
+<?php
+
+  $session = Session::getInstance();
+  $insurancePolicy = $session->getAttribute('insurancePolicy');
+  $session->removeAttribute('insurancePolicy');
+
+  if (!$insurancePolicy || !$request->getAlphaNumericParameter('accountId') ) {
+    $url = Util::buildURL($here, ADMIN_VERIFY_INSURANCE_CMD,  [ 'accountId' => $request->getAlphaNumericParameter('accountId') ]);
+    Util::jump($url);
+    exit;
+
+  } else {
+    $ebiWillHonor = $insurancePolicy->getEBIbenefits();
+    $salesforce = new SalesforceAccountManager();
+    $updated_account = $salesforce->updateInsuranceInfo($request->getAlphaNumericParameter('accountId'), $insurancePolicy);
+
+    include('templates/admin/admin.header.tem.php');
+    
+    if ($updated_account) {
+      $view['pageTitle'] = "Salesforce Account Updated";
+      echo "<div class='warningMessage'>Your Account has been updated.</div>";
+
+    } else {
+      $view['pageTitle'] = "API Error Encountered";
+      echo "<div class='errorMessage'>There was a problem updating the salesforce account. A notification was sent containing the error message.</div>";
+    }
+
+    include('templates/admin/admin.footer.tem.php');
+    exit;
+  }
+
+?>
